4405657a338fb377b6d41ef471b3ce32d92c8861
Gustavo BOBEFF <gbobeff@gismar.fr>
2022-01-19 15:17:19 +0100

    feat: add conf dev

--- src/main/java/fr/iutv/springboot/exemple_01/Exemple01Application.java
+++ src/main/java/fr/iutv/springboot/exemple_01/Exemple01Application.java
@@ -3,11 +3,17 @@ package fr.iutv.springboot.exemple_01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+import java.util.Map;
+
 @SpringBootApplication
 public class Exemple01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Exemple01Application.class, args);
+		SpringApplication app = new SpringApplication(Exemple01Application.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "4444"));
+		app.run(args);
 	}
 
 }
